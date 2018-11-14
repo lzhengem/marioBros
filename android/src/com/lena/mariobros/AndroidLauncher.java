4277b3ef67eca0a69509d6a310cdfa31b23158d6
Lena <lzhengem@gmail.com>
2018-11-13 17:49:03 -0800

    initial commit

---
+++ android/src/com/lena/mariobros/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.lena.mariobros;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.lena.mariobros.MarioBros;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new MarioBros(), config);
+	}
+}
