46b6b5d7080d81b8d67dbd9cadc0211da8789875
Lena <lzhengem@gmail.com>
2018-11-14 17:41:25 -0800

    added gameport for viewing

--- core/src/com/lena/mariobros/Screens/PlayScreen.java
+++ core/src/com/lena/mariobros/Screens/PlayScreen.java
@@ -3,17 +3,27 @@ package com.lena.mariobros.Screens;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
+import com.badlogic.gdx.utils.viewport.StretchViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
 import com.lena.mariobros.MarioBros;
 
 public class PlayScreen implements Screen {
     private MarioBros game;
     Texture texture;
+    private OrthographicCamera gamecam;
+    private Viewport gamePort;
 
     public PlayScreen(MarioBros game){
         this.game = game;
         this.texture = new Texture("badlogic.jpg");
-
+        gamecam = new OrthographicCamera();
+//        gamePort = new StretchViewport(800,490,gamecam);
+//        gamePort = new ScreenViewport(gamecam);
+        gamePort = new FitViewport(800,480,gamecam);
     }
     @Override
     public void show() {
@@ -25,6 +35,8 @@ public class PlayScreen implements Screen {
         //clear the screen
         Gdx.gl.glClearColor(1,0,0,1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        //set the camera
+        game.batch.setProjectionMatrix(gamecam.combined);
         //drawing the texture on the screen
         game.batch.begin();
         game.batch.draw(texture,0,0);
@@ -33,6 +45,8 @@ public class PlayScreen implements Screen {
 
     @Override
     public void resize(int width, int height) {
+        //when screen size changes, then resize the gameport
+        gamePort.update(width,height);
 
     }
 
