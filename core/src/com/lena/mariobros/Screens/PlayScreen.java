48b21b34d7f5c71550f46029eab6825ddf351ef7
Lena Zheng <lzhengem@gmail.com>
2018-11-15 21:20:28 -0800

    added Hud to the screen

--- core/src/com/lena/mariobros/Screens/PlayScreen.java
+++ core/src/com/lena/mariobros/Screens/PlayScreen.java
@@ -10,11 +10,13 @@ import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import com.badlogic.gdx.utils.viewport.StretchViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 import com.lena.mariobros.MarioBros;
+import com.lena.mariobros.Scenes.Hud;
 
 public class PlayScreen implements Screen {
     private MarioBros game;
     private OrthographicCamera gamecam;
     private Viewport gamePort;
+    private Hud hud;
 
     public PlayScreen(MarioBros game){
         this.game = game;
@@ -23,6 +25,7 @@ public class PlayScreen implements Screen {
 //        gamePort = new StretchViewport(800,490,gamecam); //stretches the photo to the screen
 //        gamePort = new ScreenViewport(gamecam); //picture does not change size, it will get cut off from the screen if screen gets smaller
         gamePort = new FitViewport(MarioBros.V_WIDTH,MarioBros.V_HEIGHT,gamecam); //fits the width or height onto the screen. our screen is 800 x 480
+        hud = new Hud(game.batch);
     }
     @Override
     public void show() {
@@ -32,10 +35,12 @@ public class PlayScreen implements Screen {
     @Override
     public void render(float delta) {
         //clear the screen
-        Gdx.gl.glClearColor(1,0,0,1);
+        Gdx.gl.glClearColor(0,0,0,1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-        //set the camera
-        game.batch.setProjectionMatrix(gamecam.combined);
+        //set the camera to show the hud
+        game.batch.setProjectionMatrix(hud.stage.getCamera().combined);
+        //draw the hud
+        hud.stage.draw();
     }
 
     @Override
