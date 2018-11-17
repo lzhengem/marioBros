f23c21226992794b5d9103ffcfbeb76e67dd93d6
Lena Zheng <lzhengem@gmail.com>
2018-11-16 17:07:18 -0800

    marioworld graphics on screen

--- core/src/com/lena/mariobros/Screens/PlayScreen.java
+++ core/src/com/lena/mariobros/Screens/PlayScreen.java
@@ -5,6 +5,10 @@ import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthoCachedTiledMapRenderer;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import com.badlogic.gdx.utils.viewport.StretchViewport;
@@ -18,6 +22,11 @@ public class PlayScreen implements Screen {
     private Viewport gamePort;
     private Hud hud;
 
+    private TmxMapLoader maploader; //loads in our map
+    private TiledMap map; //actual map
+    private OrthogonalTiledMapRenderer renderer; //renders map to screen
+
+
     public PlayScreen(MarioBros game){
         this.game = game;
 //        this.texture = new Texture("badlogic.jpg");
@@ -26,17 +35,38 @@ public class PlayScreen implements Screen {
 //        gamePort = new ScreenViewport(gamecam); //picture does not change size, it will get cut off from the screen if screen gets smaller
         gamePort = new FitViewport(MarioBros.V_WIDTH,MarioBros.V_HEIGHT,gamecam); //fits the width or height onto the screen. our screen is 800 x 480
         hud = new Hud(game.batch);
+
+        maploader = new TmxMapLoader();
+        map = maploader.load("level1.tmx");
+        renderer = new OrthogonalTiledMapRenderer(map);
+        //center around our viewport /2 because the default is to center at 0,0
+        gamecam.position.set(gamePort.getWorldWidth()/2,gamePort.getWorldHeight()/2,0);
     }
     @Override
     public void show() {
 
     }
+    public void handleInput(float dt){
+        if(Gdx.input.isTouched()) //if screen is being touched or finger touches phone
+            gamecam.position.x += 100 * dt;
+    }
+    //does updating of gameworld
+    public void update(float dt){
+        //check if there is any input
+        handleInput(dt);
+        //update camera everytime something happens
+        gamecam.update();
+        renderer.setView(gamecam); //only render what gamecam can see
+
+    }
 
     @Override
     public void render(float delta) {
+        update(delta);
         //clear the screen
         Gdx.gl.glClearColor(0,0,0,1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        renderer.render();
         //set the camera to show the hud
         game.batch.setProjectionMatrix(hud.stage.getCamera().combined);
         //draw the hud
