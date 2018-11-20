102f5a987ca93600f2646c28c8b73dca80ca1f25
Lena Zheng <lzhengem@gmail.com>
2018-11-20 15:59:10 -0800

    added an edgeshape for mario's head

--- core/src/com/lena/mariobros/Sprites/Mario.java
+++ core/src/com/lena/mariobros/Sprites/Mario.java
@@ -3,9 +3,11 @@ package com.lena.mariobros.Sprites;
 import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.physics.box2d.BodyDef;
 import com.badlogic.gdx.physics.box2d.CircleShape;
+import com.badlogic.gdx.physics.box2d.EdgeShape;
 import com.badlogic.gdx.physics.box2d.FixtureDef;
 import com.badlogic.gdx.physics.box2d.World;
 import com.badlogic.gdx.utils.Array;
@@ -115,6 +117,13 @@ public class Mario extends Sprite {
 
         fdef.shape = shape;
         b2body.createFixture(fdef);
+
+        EdgeShape head = new EdgeShape(); //this creates a line between 2 points
+        head.set(new Vector2(-2/MarioBros.PPM,6/MarioBros.PPM), new Vector2(2/MarioBros.PPM,6/MarioBros.PPM)); //2 behind mario, at 6 above to 2 in front of mario, 6 above
+        fdef.shape = head;
+        fdef.isSensor = true; //now it doesnt collide with anything in 2d world
+        //to be identify this fixture as the head
+        b2body.createFixture(fdef).setUserData("head");
 
     }
 }
