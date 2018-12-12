da5d4897dd74e1fadb6b786495d3094d553cea80
Lena Zheng <lzhengem@gmail.com>
2018-12-11 17:33:22 -0800

    once mario hits a coin, it changes color and get 100 points

--- core/src/com/lena/mariobros/Sprites/Coin.java
+++ core/src/com/lena/mariobros/Sprites/Coin.java
@@ -1,26 +1,32 @@
 package com.lena.mariobros.Sprites;
 
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.maps.MapObject;
-import com.badlogic.gdx.maps.objects.RectangleMapObject;
 import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TiledMapTileSet;
 import com.badlogic.gdx.math.Rectangle;
-import com.badlogic.gdx.physics.box2d.BodyDef;
-import com.badlogic.gdx.physics.box2d.FixtureDef;
-import com.badlogic.gdx.physics.box2d.PolygonShape;
 import com.badlogic.gdx.physics.box2d.World;
 import com.lena.mariobros.MarioBros;
+import com.lena.mariobros.Scenes.Hud;
 
 
 public class Coin extends InteractiveTileObject{
-    @Override
-    public void onHeadHit() {
-        Gdx.app.log("Coin", "Collision");
-    }
+    private static TiledMapTileSet tileSet;
+    private final int BLANK_COIN = 28; //it was 27 in the tileset_gutter, and libgdx counts starting at 0
+
+
 
     public Coin(World world, TiledMap map, Rectangle bounds){
         super(world, map, bounds);
+        tileSet = map.getTileSets().getTileSet("tileset_gutter");
         fixture.setUserData(this);
         setCategoryFilter(MarioBros.COIN_BIT);
+    }
+
+    @Override
+    public void onHeadHit() {
+
+        Gdx.app.log("Coin", "Collision");
+        getCell().setTile(tileSet.getTile(BLANK_COIN));
+        Hud.addScore(100);
     }
 }
