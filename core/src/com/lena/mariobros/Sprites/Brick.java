1d1d3292c2722c2a096a52513bf3049bc5037743
Lena Zheng <lzhengem@gmail.com>
2018-12-11 17:28:44 -0800

    hitting bricks give 200 points

--- core/src/com/lena/mariobros/Sprites/Brick.java
+++ core/src/com/lena/mariobros/Sprites/Brick.java
@@ -5,6 +5,7 @@ import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.physics.box2d.World;
 import com.lena.mariobros.MarioBros;
+import com.lena.mariobros.Scenes.Hud;
 
 public class Brick extends InteractiveTileObject {
 
@@ -21,5 +22,6 @@ public class Brick extends InteractiveTileObject {
         Gdx.app.log("Brick", "Collision");
         setCategoryFilter(MarioBros.DESTROYED_BIT);
         getCell().setTile(null);
+        Hud.addScore(200);
     }
 }
