30099b6c01cf47f10d9d54c3137a35c34dfc32a7
Lena <lzhengem@gmail.com>
2018-12-14 15:50:47 -0800

    hitting brick, coins, and empty coin has sounds

--- core/src/com/lena/mariobros/Sprites/Coin.java
+++ core/src/com/lena/mariobros/Sprites/Coin.java
@@ -1,6 +1,7 @@
 package com.lena.mariobros.Sprites;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TiledMapTileSet;
 import com.badlogic.gdx.math.Rectangle;
@@ -26,7 +27,13 @@ public class Coin extends InteractiveTileObject{
     public void onHeadHit() {
 
         Gdx.app.log("Coin", "Collision");
+        if(getCell().getTile().getId() == BLANK_COIN)
+            MarioBros.manager.get("audio/sounds/bump.wav", Sound.class).play();
+        else
+            MarioBros.manager.get("audio/sounds/coin.wav", Sound.class).play();
+
         getCell().setTile(tileSet.getTile(BLANK_COIN));
         Hud.addScore(100);
+
     }
 }
