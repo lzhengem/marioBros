d34328ed15aaf6fe0ccc1d363abf1eb5cb0f887d
Lena <lzhengem@gmail.com>
2018-12-15 17:22:25 -0800

    created enemy class and goomba class

---
+++ core/src/com/lena/mariobros/Sprites/Goomba.java
@@ -0,0 +1,15 @@
+package com.lena.mariobros.Sprites;
+
+import com.lena.mariobros.Screens.PlayScreen;
+
+public class Goomba extends Enemy {
+
+    public Goomba(PlayScreen screen, float x, float y) {
+        super(screen, x, y);
+    }
+
+    @Override
+    protected void defineEnemy() {
+
+    }
+}
